761fe45292d4c4d113a144a0a79ca52a7ee6fed8
John Madison <johnmadison@NUNYA-BIZNAZZ.local>
2014-11-20 13:34:23 -0700

    Initial Commit

---
+++ Program#7/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Program#7
+//
+//  Created by John Madison on 11/20/14.
+//  Copyright (c) 2014 cmu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
